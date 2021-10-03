695e1c23b4be1806caa07f39b712f6bdaff6bb99
Kate Hubkina <ekaterinahubkina@yandex.by>
2021-10-03 22:53:09 +0300

    исправлены ошибки после 1 ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,31 +1,43 @@
-const popup = document.querySelector('.pop-up');
-const popupCloseButton = document.querySelector('.pop-up__close-button');
+const popup = document.querySelector('.popup');
+const popupCloseButton = document.querySelector('.popup__close-button');
 const editButton = document.querySelector('.profile__edit-button');
-const formButton = document.querySelector('.form__button');
 
 const form = document.querySelector('.form');
 
 const profileName = document.querySelector('.profile__name');
-const inputName = document.querySelector('.form__input_name');
+const inputName = document.querySelector('.form__input_type_name');
 const profileOccupation = document.querySelector('.profile__occupation');
-const inputOccupation = document.querySelector('.form__input_occupation');
+const inputOccupation = document.querySelector('.form__input_type_occupation');
 
 function openPopup() {
-    popup.classList.add('pop-up_opened')
+    popup.classList.add('popup_opened')
 };
 
 function closePopup() {
-    popup.classList.remove('pop-up_opened')
+    popup.classList.remove('popup_opened')
 };
 
 editButton.addEventListener('click', openPopup);
 popupCloseButton.addEventListener('click', closePopup);
 
+inputName.value = profileName.textContent;
+inputOccupation.value = profileOccupation.textContent;
+
 function submitForm(event) {
     event.preventDefault()
     profileName.textContent = inputName.value;
     profileOccupation.textContent = inputOccupation.value;
-    console.log(profileOccupation, profileName);
     closePopup();
 }
 form.addEventListener('submit', submitForm);
+
+//like-button_active
+
+// const likeButton = document.querySelectorAll('.element__like-button');
+// console.log(likeButton);
+
+// likeButton.forEach(function(elem) {
+//     elem.addEventListener('click', function(){
+//         this.classList.add('element__like-button_active');
+//     })
+// });
