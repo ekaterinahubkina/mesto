492db1800276afe4333fc591a82c278c614bf794
Kate Hubkina <ekaterinahubkina@yandex.by>
2021-10-15 23:28:40 +0300

    creater new popup

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,9 @@
-const popup = document.querySelector('.popup');
-const popupCloseButton = document.querySelector('.popup__close-button');
+const popupEdit = document.querySelector('.popup_type_edit');
+const popupAdd = document.querySelector('.popup_type_add'); // добавили новый попап новое место
+const popupCloseButtonEdit = document.querySelector('.popup__close-button_type_edit');
+const popupCloseButtonAdd = document.querySelector('.popup__close-button_type_add');
 const editButton = document.querySelector('.profile__edit-button');
+const addButton = document.querySelector('.profile__add-button'); //добавили кнопку добавить
 
 const form = document.querySelector('.form');
 
@@ -9,16 +12,28 @@ const inputName = document.querySelector('.form__input_type_name');
 const profileOccupation = document.querySelector('.profile__occupation');
 const inputOccupation = document.querySelector('.form__input_type_occupation');
 
-function openPopup() {
+function openPopup(popup) {
     popup.classList.add('popup_opened')
 };
 
-function closePopup() {
+function closePopup(popup) {
     popup.classList.remove('popup_opened')
 };
 
-editButton.addEventListener('click', openPopup);
-popupCloseButton.addEventListener('click', closePopup);
+editButton.addEventListener('click', (event) => {
+    openPopup(popupEdit);
+});
+popupCloseButtonEdit.addEventListener('click', (event) => {
+    closePopup(popupEdit);
+});
+
+addButton.addEventListener('click', (event) => {
+    openPopup(popupAdd);
+}); //открытие поп-апа по нажатию на кнопку добавить
+
+popupCloseButtonAdd.addEventListener('click', (event) => {
+    closePopup(popupAdd);
+});
 
 inputName.value = profileName.textContent;
 inputOccupation.value = profileOccupation.textContent;
@@ -27,6 +42,6 @@ function submitForm(event) {
     event.preventDefault()
     profileName.textContent = inputName.value;
     profileOccupation.textContent = inputOccupation.value;
-    closePopup();
+    closePopup(popupEdit);
 }
 form.addEventListener('submit', submitForm);
