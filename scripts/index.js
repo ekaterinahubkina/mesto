fac8b30e807c76a6e0037d8fb3bc49dda29b5446
Kate Hubkina <ekaterinahubkina@yandex.by>
2021-10-17 00:14:43 +0300

    added add-card feature

--- scripts/index.js
+++ scripts/index.js
@@ -5,8 +5,12 @@ const popupCloseButtonAdd = document.querySelector('.popup__close-button_type_ad
 const editButton = document.querySelector('.profile__edit-button');
 const addButton = document.querySelector('.profile__add-button'); //добавили кнопку добавить
 
-const form = document.querySelector('.form');
 
+const formAdd = document.querySelector('.form_type_add');
+const inputTitle = document.querySelector('.form__input_type_title');
+const inputLink = document.querySelector('.form__input_type_link');
+
+const form = document.querySelector('.form');
 const profileName = document.querySelector('.profile__name');
 const inputName = document.querySelector('.form__input_type_name');
 const profileOccupation = document.querySelector('.profile__occupation');
@@ -45,3 +49,72 @@ function submitForm(event) {
     closePopup(popupEdit);
 }
 form.addEventListener('submit', submitForm);
+
+
+// добавление карточек
+const initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+  ];
+
+const cardsSection = document.querySelector('.cards'); // добавили секцию Cards
+const templateItem = document.querySelector('.template').content; // добавили template
+
+initialCards.forEach(addCard); 
+
+function createCard(item) {
+
+  const card = templateItem.querySelector('.card').cloneNode(true);
+  card.querySelector('.card__title').innerText = item.name;
+  card.querySelector('.card__image').src = item.link;
+  const deleteButton = card.querySelector('.card__delete-button');
+  deleteButton.addEventListener('click', (event) =>{
+    event.target.closest('.card').remove();
+  });
+  const likeButton = card.querySelector('.card__like-button');
+  likeButton.addEventListener('click', (event) => {
+    likeButton.classList.toggle('card__like-button_active');
+  });
+  return card;
+};
+
+function addCard(item) {
+  const card = createCard(item);
+  cardsSection.prepend(card);
+}
+
+function addCardSubmit(event) {
+  event.preventDefault();
+  const name = inputTitle.value;
+  const link = inputLink.value;
+  const item = {
+    name: name,
+    link: link
+  };
+  addCard(item);
+  event.target.reset();
+  closePopup(popupAdd);
+}
+formAdd.addEventListener('submit', addCardSubmit);
