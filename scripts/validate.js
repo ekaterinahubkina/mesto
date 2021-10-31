bd07f737032acfafd3b7bd17d27f43f177b8f7b3
Kate Hubkina <ekaterinahubkina@yandex.by>
2021-10-31 21:51:23 +0300

    added validation

---
+++ scripts/validate.js
@@ -0,0 +1,61 @@
+const validationConfig = {
+    formSelector: '.form',
+    inputSelector: '.form__input',
+    submitButtonSelector: '.form__button',
+    inactiveButtonClass: 'form__button_disabled',
+    inputErrorClass: 'form__input_type_error',
+    errorClass: 'form__error_visible'
+}
+
+function enableValidation(config) {
+    const forms = Array.from(document.querySelectorAll(config.formSelector));
+    forms.forEach(function (form){
+        setFormListeners(form, config)
+    });
+}
+
+function setFormListeners(form, config){
+    form.addEventListener('input', function (){
+        toggleButtonState(form, config);
+    });
+    const inputs = Array.from(form.querySelectorAll(config.inputSelector));
+    inputs.forEach(function (input){
+        input.addEventListener('input', function (){
+            checkInputValidity(input, form, config);
+        });
+    });
+    toggleButtonState(form, config);
+}
+
+function checkInputValidity(input, form, config){
+    if(!input.validity.valid){
+        showError(input, form, config);
+    } else {
+        hideError(input, form, config);
+    }
+}
+
+function showError(input, form, config){
+    const error  = form.querySelector(`.${input.id}-error`);
+    input.classList.add(config.inputErrorClass);
+    error.classList.add(config.errorClass);
+    error.textContent = input.validationMessage;
+}
+
+function hideError(input, form, config){
+    const error  = form.querySelector(`.${input.id}-error`);
+    input.classList.remove(config.inputErrorClass);
+    error.classList.remove(config.errorClass);
+    error.textContent = '';
+}
+
+function toggleButtonState(form, config){
+    const button = form.querySelector(config.submitButtonSelector);
+    if(!form.checkValidity()){
+        button.classList.add(config.inactiveButtonClass);
+    } else {
+        button.classList.remove(config.inactiveButtonClass);
+    }
+}
+enableValidation(validationConfig);
+
