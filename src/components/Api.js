64c0d9a5f305ee7953bbab08d147542631954d15
Kate Hubkina <ekaterinahubkina@yandex.by>
2021-12-17 23:47:47 +0300

    add cards from api

---
+++ src/components/Api.js
@@ -0,0 +1,60 @@
+class Api{
+    constructor({url, token}) {
+        this.url = url;
+        this.token = token;
+    }
+
+    getCards() {
+        return fetch(`${this.url}/cards`, {
+            headers: {
+                authorization: this.token
+            }
+        })
+            .then(res => {
+                if (res.ok) {
+                    return res.json();
+                } else {
+                    return Promise.reject(`Ошибка: ${res.status}`);
+                }
+
+            })
+    }
+
+    getUserData() {
+        return fetch(`${this.url}/users/me`, {
+            headers: {
+                authorization: this.token
+            }
+        })
+            .then(res => {
+                if (res.ok) {
+                    return res.json();
+                } else {
+                    return Promise.reject(`Ошибка: ${res.status}`);
+                }
+        })
+    }
+
+    addNewCard(data) {
+        return fetch(`${this.url}/cards`, {
+            method: 'POST',
+            headers: {
+                authorization: this.token,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                name: data.name,
+                link: data.link
+            })
+        })
+            .then(res => {
+                if (res.ok) {
+                    return res.json();
+                } else {
+                    return Promise.reject(`Ошибка при добавлении карточки: ${res.status}`);
+                }
+            })
+    }
+}
+
+export default Api
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,30 @@ import Section from "../components/Section.js";
 import PopupWithForm from "../components/PopupWithForm.js";
 import UserInfo from "../components/UserInfo.js";
 import PopupWithImage from "../components/PopupWithImage.js";
+import Api from "../components/Api.js";
 import { editButton, addButton, formEdit, formAdd, inputName, inputOccupation, userName, userOccupation } from "../utils/constants.js";
+
+const api = new Api({
+    url: 'https://mesto.nomoreparties.co/v1/cohort-32',
+    token: '76c1c471-2766-4a3c-9dbb-2acf0a9ae808'
+});
+
+api.getUserData()
+    .then(data => {
+        userName.textContent = data.name;
+        userOccupation.textContent = data.about;
+    })
+    .catch(error => {
+        console.log(error)
+    })
+
+api.getCards()
+    .then(res => {
+        initialCardsList.renderItems(res)
+    })
+    .catch(error => {
+        console.log(error)
+    })
 
 function createCard(item) {
     const card = new Card(item, '.template', handleCardClick);
@@ -15,19 +38,23 @@ function createCard(item) {
 }
 
 const initialCardsList = new Section({
-    items: initialCards,
     renderer: (item) => {
-        // const card = new Card(item, '.template', handleCardClick);
-        // const cardElement = card.generateCard();
         initialCardsList.addItem(createCard(item));
     }},
     '.cards');
 
-initialCardsList.renderItems();
+//initialCardsList.renderItems();
 
 const popupAdd = new PopupWithForm({
     popupSelector: '.popup_type_add',
     handleFormSubmit: (formData) => {
+        api.addNewCard(formData)
+            .then(res => {
+                console.log(res)
+            })
+            .catch(error => {
+                console.log(error)
+            })
         initialCardsList.addItem(createCard(formData));
     }
 });
@@ -38,20 +65,20 @@ addButton.addEventListener('click', () => {
     formAddValidator.resetForm();
 });
 
-function setInitialUserInfo() {
-    fetch('https://mesto.nomoreparties.co/v1/cohort-32/users/me', {
-        headers: {
-            authorization: '76c1c471-2766-4a3c-9dbb-2acf0a9ae808'
-        }
-    })
-        .then(res => res.json())
-        .then((result) => {
-            console.log(result);
-            userName.textContent = result.name;
-            userOccupation.textContent = result.about;
-        });
-}
-setInitialUserInfo();
+// function setInitialUserInfo() {
+//     fetch('https://mesto.nomoreparties.co/v1/cohort-32/users/me', {
+//         headers: {
+//             authorization: '76c1c471-2766-4a3c-9dbb-2acf0a9ae808'
+//         }
+//     })
+//         .then(res => res.json())
+//         .then((result) => {
+//             console.log(result);
+//             userName.textContent = result.name;
+//             userOccupation.textContent = result.about;
+//         });
+// }
+// setInitialUserInfo();
 
 const userInfo = new UserInfo({
     nameSelector: '.profile__name',
